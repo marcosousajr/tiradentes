1ec4198b515b54ec89c84f37d7a6013dd811411c
Marcos Aurélio Jr. <marcosaurelio88@gmail.com>
2017-05-14 00:20:45 -0300

    Leitura de arquivo - ok verificar se rg já foi inserido - fail contar tempo de inserção - ok método contém - fail comentários emais alguma coisa que esqueci.

--- br.unit.ed2/src/me2/Main.java
+++ br.unit.ed2/src/me2/Main.java
@@ -1,19 +1,24 @@
 package me2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import me.Keyboard;
 import me2.Rg;
 import me2.QueryHashTable;
 
 public class Main {
-	
-	
-	static QueryHashTable alunos = new QueryHashTable();
-	
+
+	private static QueryHashTable alunos = new QueryHashTable();
+	private static final String FILENAME = "//mnt//sda4//listaTeste.txt";
+	private static int cont = 0;
+
 	public static void main(String[] args) {
-		int cont = 0;
+
 		int opcao;
 		do {
-			Keyboard.clrscr();
+			// Keyboard.clrscr();
 			opcao = Keyboard.menu("Inserir Objeto/Buscar Objeto/Listar Objeto/Remover Objeto");
 			switch (opcao) {
 			case 1:
@@ -29,39 +34,53 @@ public class Main {
 				remover();
 				break;
 			}
-			
+
 		} while (opcao < 5);
 		System.out.println("\nFim do programa");
-		
+
 	}
-	
-	static void inserir(){
-		Aluno aluno = new Aluno();
-		String rgAluno = Keyboard.readString("Digite o rg");
-		Rg rg = new Rg(rgAluno);
-		String nome = Keyboard.readString("Digite o nome do aluno");
-		aluno.setRg(rg);
-		aluno.setNome(nome);
-		alunos.inserir(rg, aluno);
+
+	static void inserir() {
+		long temIni = System.currentTimeMillis();
+		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+			String linhaAtual;
+			Aluno aluno = new Aluno();
+			while ((linhaAtual = br.readLine()) != null) {
+				String[] textoSeparado = linhaAtual.split(";|;\\s");
+				String rgAluno = textoSeparado[0];
+				String nome = textoSeparado[1];
+				Rg rg = new Rg(rgAluno);
+				aluno.setRg(rg);
+				aluno.setNome(nome);
+				alunos.inserir(rg, aluno);
+				cont++;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		long temFim = System.currentTimeMillis();
+		System.out.println("Tempo total Inserir " + (temFim - temIni));
+		System.out.println("Nº de inserções : " + cont);
 	}
-	
-	static void buscar(){
-		if(alunos != null){
+
+	static void buscar() {
+		if (alunos != null) {
 			String rgAluno = Keyboard.readString("Digite o rg");
 			Rg rg = new Rg(rgAluno);
 			alunos.buscar(rg);
 		}
 		System.out.println("Tabela Vazia!");
 	}
-	
-	static void listar(){
+
+	static void listar() {
 		if (alunos != null) {
 			alunos.listar();
 		}
 		System.out.println("Tabela Vazia!");
 	}
-	
-	static void remover(){
+
+	static void remover() {
 		if (alunos != null) {
 			String rgAluno = Keyboard.readString("Digite o rg");
 			Rg rg = new Rg(rgAluno);
