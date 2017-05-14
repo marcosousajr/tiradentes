25747893f8a994fd69028ba8768f70ef89410c4b
Marcos Aurélio Jr. <marcosaurelio88@gmail.com>
2017-05-14 14:05:59 -0300

    Corrigindo o insert - ok

--- br.unit.ed2/src/me2/Main.java
+++ br.unit.ed2/src/me2/Main.java
@@ -4,21 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-import me.Keyboard;
+import me2.Keyboard;
 import me2.Rg;
 import me2.QueryHashTable;
 
 public class Main {
 
 	private static QueryHashTable alunos = new QueryHashTable();
-	private static final String FILENAME = "//mnt//sda4//listaTeste.txt";
-	private static int cont = 0;
+	private static final String INSERT = "//mnt//sda4//listInsert.txt";
+	private static final String SEARCH = "//mnt//sda4//listSearch.txt";
 
 	public static void main(String[] args) {
 
 		int opcao;
 		do {
-			// Keyboard.clrscr();
+			Keyboard.clrscr();
 			opcao = Keyboard.menu("Inserir Objeto/Buscar Objeto/Listar Objeto/Remover Objeto");
 			switch (opcao) {
 			case 1:
@@ -42,16 +42,15 @@ public class Main {
 
 	static void inserir() {
 		long temIni = System.currentTimeMillis();
-		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+		int cont=0;
+		try (BufferedReader br = new BufferedReader(new FileReader(INSERT))) {
 			String linhaAtual;
 			Aluno aluno = new Aluno();
 			while ((linhaAtual = br.readLine()) != null) {
 				String[] textoSeparado = linhaAtual.split(";|;\\s");
-				String rgAluno = textoSeparado[0];
-				String nome = textoSeparado[1];
-				Rg rg = new Rg(rgAluno);
+				Rg rg = new Rg(textoSeparado[0]);
 				aluno.setRg(rg);
-				aluno.setNome(nome);
+				aluno.setNome(textoSeparado[1]);
 				alunos.inserir(rg, aluno);
 				cont++;
 			}
@@ -62,31 +61,60 @@ public class Main {
 		long temFim = System.currentTimeMillis();
 		System.out.println("Tempo total Inserir " + (temFim - temIni));
 		System.out.println("Nº de inserções : " + cont);
+		System.out.println("Tamanho da tabela : " + alunos.tamanho());
+		
+		Keyboard.waitEnter();
 	}
 
-	static void buscar() {
-		if (alunos != null) {
-			String rgAluno = Keyboard.readString("Digite o rg");
-			Rg rg = new Rg(rgAluno);
-			alunos.buscar(rg);
+	static void buscar() { //não encontrou!
+		long temIni = System.currentTimeMillis();
+		int cont=0;
+		try (BufferedReader br = new BufferedReader(new FileReader(SEARCH))) {
+			String linhaAtual;
+
+			if (alunos != null) {
+				while ((linhaAtual = br.readLine()) != null) {
+					String[] textoSeparado = linhaAtual.split(";|;\\s");
+					Rg rg = new Rg(textoSeparado[0]);
+					alunos.buscar(rg);
+					cont++;
+				}
+			}
+			System.out.println("Tabela Vazia!");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		System.out.println("Tabela Vazia!");
+
+		long temFim = System.currentTimeMillis();
+		System.out.println("Tempo total Buscar " + (temFim - temIni));
+		System.out.println("Nº de Buscas : " + cont);
+		Keyboard.waitEnter();
 	}
 
 	static void listar() {
+		long temIni = System.currentTimeMillis();
+		
 		if (alunos != null) {
-			alunos.listar();
+			alunos.listar(); //listando apenas o último
 		}
 		System.out.println("Tabela Vazia!");
+		long temFim = System.currentTimeMillis();
+		System.out.println("Tempo total Listar " + (temFim - temIni));
+		Keyboard.waitEnter();
+
 	}
 
 	static void remover() {
+		long temIni = System.currentTimeMillis();
 		if (alunos != null) {
 			String rgAluno = Keyboard.readString("Digite o rg");
 			Rg rg = new Rg(rgAluno);
 			alunos.remover(rg);
 		}
 		System.out.println("Tabela Vazia!");
+		long temFim = System.currentTimeMillis();
+		System.out.println("Tempo total Remover " + (temFim - temIni));
+		Keyboard.waitEnter();
 	}
 
 }
--- br.unit.ed2/src/me2/QueryHashTable.java
+++ br.unit.ed2/src/me2/QueryHashTable.java
@@ -14,67 +14,58 @@ public class QueryHashTable {
 	public QueryHashTable() {
 		this.estrutura = new Hashtable<>();
 	}
-	
-	//MÉTODOS AUXILIARES//
-	
-	public Boolean contem(Object chave){
-		if (buscar(chave)!=null) {
+
+	// MÉTODOS AUXILIARES//
+
+	public Boolean contem(Object chave) {
+		if (this.estrutura.containsKey(chave))
 			return true;
-		}
 		return false;
 	}
 	
-	//#################//
-	
-	//MÉTODOS PEDIDOS//
-	
+	public int tamanho(){
+		return this.estrutura.size();
+	}
+
+	// #################//
+
+	// MÉTODOS PEDIDOS//
+
 	public Boolean inserir(Object chave, Object valor) {
-		//long temIni = System.currentTimeMillis();
-		if (!contem(chave)) { //ESSA VERIFICAÇÃO NÃO TA FUNCIONANDO!
+		if (!contem(chave)) { // ESSA VERIFICAÇÃO NÃO TA FUNCIONANDO!
 			this.estrutura.put(chave, valor);
 			return true;
 		}
-		//long temFim = System.currentTimeMillis();
-		//System.out.println("Tempo total Inserir " + (temFim - temIni));
 		return false;
 	}
 
 	public Object buscar(Object chave) {
-		//long temIni = System.currentTimeMillis();
 		Object retorno = this.estrutura.get(chave);
-		//long temFim = System.currentTimeMillis();
-		//System.out.println("Tempo total Buscar " + (temFim - temIni));
 		return retorno;
 	}
 
 	public void listar() {
-		long temIni = System.currentTimeMillis();
 		Enumeration<Object> keys = this.estrutura.keys();
 		while (keys.hasMoreElements())
 			System.out.println(this.estrutura.get(keys.nextElement()));
-		long temFim = System.currentTimeMillis();
-		System.out.println("Tempo total Listar " + (temFim - temIni));
-	}
+			}
 
 	public Boolean remover(Object chave) {
-		long temIni = System.currentTimeMillis();
 		if (this.estrutura.containsKey(chave)) {
 			this.estrutura.remove(chave);
 		}
-		long temFim = System.currentTimeMillis();
-		System.out.println("Tempo total Remover " + (temFim - temIni));
 		return false;
 	}
-	
-	public Boolean inserirBloco(Object[][] valores){
+
+	public Boolean inserirBloco(Object[][] valores) {
 		long temIni = System.currentTimeMillis();
-		for(int i = 0; i < valores.length; i++){
+		for (int i = 0; i < valores.length; i++) {
 			this.inserir(valores[i][0], valores[i][1]);
 		}
 		long temFim = System.currentTimeMillis();
 		System.out.println("Tempo total Inserção Bloco " + (temFim - temIni));
 		return true;
 	}
-	
-	//#################//
+
+	// #################//
 }
